4e21d667dcf028c3719f6de3c44440ea8ca84acc
Garuman <40666406+Garuman@users.noreply.github.com>
2021-06-14 00:20:12 +0200

    Update group_norm.py

--- group_norm.py
+++ group_norm.py
@@ -5,7 +5,6 @@ from keras import initializers
 from keras import regularizers
 from keras import constraints
 from keras import backend as K
-#
 from keras.utils.generic_utils import get_custom_objects
 
 
